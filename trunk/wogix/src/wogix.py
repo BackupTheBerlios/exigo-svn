bd71022d9b2301f9184f8433b5661e77e2486fe0
alex_extreme <alex_extreme@4ff39de8-5400-0410-838c-a0cc6bf4a457>
2005-09-25 08:12:05 +0000

    Xlib implementation

--- trunk/wogix/src/wogix.py
+++ trunk/wogix/src/wogix.py
@@ -3,9 +3,32 @@
 # Wogix - Tabbed Window Manager for Exigo
 # Copyright (C) 2005 Alex Smith <alex.extreme2 at gmail.com>
 
+# Import our stuff
+
+import sys
+import os
+import signal
+import errno
+import array
+import traceback
+import re
+import string
+import types
+
+# Import Xlib
+
+from Xlib import display, X, Xutil, Xatom, rdb, error
+import Xlib.protocol.event
+
+# Import our version info
+
 import version
 
 print "w00t0h!"
 print "Exigo is born :D"
 print
 print "Welcome to " + version.appName + " v" + version.appVer + "!"
+
+# Create the Display object
+xDisplay = display.Display()
+
